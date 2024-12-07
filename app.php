bc86176a528183d2387f9f705f294a7e1fe75886
KuliAztec <muhammad.setyoko@mhs.unsoed.ac.id>
2024-12-08 02:04:19 +0700

    refactoring

--- app.php
+++ app.php
@@ -6,7 +6,7 @@ session_start();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Acak Aja</title>
+  <title>Randomizing</title>
   <style>
     body {
       font-family: Arial, sans-serif;
